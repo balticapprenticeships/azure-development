c09b134a599aa1d2835a45b2eff696ea23b481d7
Chris Langford <chris.langford@lang-ford.uk>
2023-06-16 13:33:06 +0100

    added new file

---
+++ Extensions/install-nvidia-drivers.ps1
@@ -0,0 +1,26 @@
+#region Power Bi Desktop
+try {
+    #$powerBi = "Microsoft Power BI Desktop"
+    #$powerBiInstalled = ((Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*) | Where-Object { $_.DisplayName -like "*$powerBi*" })
+    #if($powerBiInstalled){
+       # Write-Log "Microsoft Power BI Desktop is already install"
+        #Write-Output "Microsoft Power BI Desktop is already install"
+    #}else{
+       # Write-Log "Installing Nvidia Drivers"
+        Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=874181" -OutFile C:\NvidiaGRID-15-2.exe
+        Write-Output "Installing Nvidia Drivers"
+        $nvidiaArgs = @('/s')
+        Start-Process -FilePath "C:\buildArtifacts\windows-client\PBIDesktopSetup_x64.exe" -ArgumentList $nvidiaArgs -Wait
+        #if($powerBiInstalled){
+           # Write-Log "Microsoft Power BI Desktop installation succeded"
+        #    Write-Output "Microsoft Power BI Desktop installation succeded"
+        #}
+    #}
+}
+catch [System.Exception] {
+    Write-Warning $_.Exception.Message
+    #Write-Log "Error installing Microsoft Power BI Desktop: $ErrorMessage"
+    Write-Output "Error installing Microsoft Power BI Desktop: $ErrorMessage"
+}
+
+#endregion
